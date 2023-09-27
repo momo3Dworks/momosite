df4480ce04e1e71837aa81d6e0a599df8dc84487
momotaroking <119371383+momo3Dworks@users.noreply.github.com>
2023-09-27 02:51:16 -0300

    19Up

---
+++ precache-manifest.a687d11aa484a3c9ffdb0a11a541a9ef.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "88beb9c3da75a776092b",
+    "url": "/static/css/main.78d86af2.chunk.css"
+  },
+  {
+    "revision": "88beb9c3da75a776092b",
+    "url": "/static/js/main.88beb9c3.chunk.js"
+  },
+  {
+    "revision": "3af03192f83efab4825f",
+    "url": "/static/js/1.3af03192.chunk.js"
+  },
+  {
+    "revision": "a4c770b8a2028a938d2c",
+    "url": "/static/css/2.dd991f99.chunk.css"
+  },
+  {
+    "revision": "a4c770b8a2028a938d2c",
+    "url": "/static/js/2.a4c770b8.chunk.js"
+  },
+  {
+    "revision": "4b3cdeaebd06d36ecaa0",
+    "url": "/static/js/runtime~main.4b3cdeae.js"
+  },
+  {
+    "revision": "f2dda4a66a0e6be2af0127bb46589024",
+    "url": "/static/media/0xgpt_logoF.f2dda4a6.svg"
+  },
+  {
+    "revision": "64a77321d08dbcee44928f7dd591b4c7",
+    "url": "/static/media/twitter.64a77321.svg"
+  },
+  {
+    "revision": "14ba6182e8c9194c5e8d7fdafdc0fad7",
+    "url": "/static/media/telegram.14ba6182.svg"
+  },
+  {
+    "revision": "34afa328350d9d0f50d145c8e0de02b9",
+    "url": "/static/media/infinitePOS.34afa328.mp4"
+  },
+  {
+    "revision": "da6307a41de240fea5968cd029249627",
+    "url": "/static/media/0006.da6307a4.webp"
+  },
+  {
+    "revision": "c6d180dcd45730284bad2fbece68e31b",
+    "url": "/static/media/0028.c6d180dc.webp"
+  },
+  {
+    "revision": "9aa83d606e5a0bf1a6624c5317f383b7",
+    "url": "/static/media/DescriptionLazy.68743418.jpg"
+  },
+  {
+    "revision": "456523d054f81e3f0069d3407f0b734b",
+    "url": "/static/media/LogoLazy.b37d51e5.jpg"
+  },
+  {
+    "revision": "cc127a063891cbcd1d0ebcc1b8005181",
+    "url": "/static/media/earthorbiter.cc127a06.ttf"
+  },
+  {
+    "revision": "0541b0c8c1fa38a69fecca3d125c81ed",
+    "url": "/static/media/Exo2-VariableFont_wght.0541b0c8.ttf"
+  },
+  {
+    "revision": "5d5b92fe3045589ab9b0bc3d59a76a62",
+    "url": "/static/media/BackgroundFuture.5d5b92fe.webp"
+  },
+  {
+    "revision": "fa49dbf7551f978467b216a62501cfac",
+    "url": "/static/media/Noise1.c56c7a4c.jpg"
+  },
+  {
+    "revision": "f8c2d7f9837ab3aa568c6205333cfc52",
+    "url": "/static/media/backgroundAR.1cd91c28.png"
+  },
+  {
+    "revision": "55421ef277667e57d80c96deb4bba062",
+    "url": "/static/media/PAGE1.55421ef2.webp"
+  },
+  {
+    "revision": "81b03db7b25e84ada47c1867fd729cb3",
+    "url": "/static/media/lux.81b03db7.mp4"
+  },
+  {
+    "revision": "fe2bff6394e4513488f39d35ad1c044b",
+    "url": "/static/media/medium.fe2bff63.svg"
+  },
+  {
+    "revision": "d3ba3abf8e84510d00bdd3cc177370f3",
+    "url": "/static/media/pieChart.ba5bc8a7.png"
+  },
+  {
+    "revision": "0da96c7374d04f6147cebe750b9df66a",
+    "url": "/static/media/background3.c5a9969f.png"
+  },
+  {
+    "revision": "c2a76e5188193639f9dde293bf9be690",
+    "url": "/static/media/background4.162f53af.png"
+  },
+  {
+    "revision": "4f547eb3c1e5d79a23091cd3c94ad4c7",
+    "url": "/static/media/background5.a764a832.png"
+  },
+  {
+    "revision": "ae73e024c9dff8357f5d428a60b490b1",
+    "url": "/static/media/background6.ff8b8b7e.png"
+  },
+  {
+    "revision": "ae73e024c9dff8357f5d428a60b490b1",
+    "url": "/static/media/background7.ff8b8b7e.png"
+  },
+  {
+    "revision": "07af3ff791b5094027377b03dc48713c",
+    "url": "/static/media/MoMoWorksLogo.dac3d7a5.png"
+  },
+  {
+    "revision": "83507dd415a2b524bcb7a12c6d88bfda",
+    "url": "/index.html"
+  }
+];
